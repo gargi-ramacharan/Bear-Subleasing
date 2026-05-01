e04855c9c807d916d152682bb79c45891f503c14
gargi <gargiramacharan@berkeley.edu>
2026-04-30 18:04:44 -0700

    fixed listings.js

--- server/models/listings.js
+++ server/models/listings.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const listingSchema = new mongoose.Schema({
   title: { type: String, required: true },
   price: { type: Number, required: true },
-  location: { type: String, enum: ['northside', 'southside'], required: true },
+  location: { type: String, required: true }, 
   description: { type: String },
   images: [String],
-  contactEmail: { type: String, required: true },
+  contactEmail: { type: String, required: true }, 
   available: { type: Boolean, default: true },
 }, { timestamps: true });
 
